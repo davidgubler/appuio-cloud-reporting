a93924164cc4afa7bc7133c24723b36b0eafe0be
Sebastian Widmer <sebastian.widmer@vshn.net>
2022-01-12 17:59:16 +0100

    More work on the report

--- pkg/report/report.go
+++ pkg/report/report.go
@@ -2,8 +2,10 @@ package report
 
 import (
 	"context"
+	"fmt"
 	"time"
 
+	"github.com/appuio/appuio-cloud-reporting/pkg/db"
 	"github.com/jmoiron/sqlx"
 	apiv1 "github.com/prometheus/client_golang/api/prometheus/v1"
 	"github.com/prometheus/common/model"
@@ -13,6 +15,19 @@ type PromQuerier interface {
 	Query(ctx context.Context, query string, ts time.Time) (model.Value, apiv1.Warnings, error)
 }
 
-func Run(db *sqlx.DB, prom PromQuerier, queryName string, ts time.Time) error {
+func Run(dbx *sqlx.DB, prom PromQuerier, queryName string, ts time.Time) error {
+	var query db.Query
+	err := sqlx.Get(dbx, &query, "SELECT * FROM queries WHERE name = $1 AND (during @> $2::timestamptz)", queryName, ts)
+	if err != nil {
+		return err
+	}
+
+	res, warn, err := prom.Query(context.TODO(), query.Query, ts)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(res, warn)
+
 	return nil
 }
